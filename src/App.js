4ce189ef75627f1e8679a19d2a89d6ec8532f4d9
Theo Okafor <theo.okafor@yahoo.com>
2020-01-24 12:16:17 +0100

    add axios for fetching data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+
 import './App.css'
 import Calendar from './container/calendar';
 import data from './utils/data.json';
@@ -25,39 +27,45 @@ const prepCalendar = () => {
 }
 prepCalendar();
 
-function App() {
-  return (
-    <div className="App">
-      <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <h2 className="">Mini Calendly</h2>
-          <span role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </span>
-        </div>
-      </nav>
-      <section className="section">
-        <div className="container">
-          <div className="">
+class App extends Component {
+  async componentDidMount() {
+    const data = await axios.get('http://127.0.0.1:8000/api/mentor/1');
+    console.log(data);
+  }
+  render(){
+    return (
+      <div className="App">
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+          <div className="navbar-brand">
+            <h2 className="">Mini Calendly</h2>
+            <span role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </span>
+          </div>
+        </nav>
+        <section className="section">
+          <div className="container">
+            <div className="">
 
-          </div>
-          <div className="columns">
-            <div className="column is-2" >
-              <p>{dataObj.mentor.name}</p>
-              <h2>1 hour meeting</h2>
-              <h6>GMT {dataObj.mentor['time_zone']}</h6>
             </div>
-            <div className="column is-8 card">
-              <Calendar data={calendarData} />
+            <div className="columns">
+              <div className="column is-2" >
+                <p>{dataObj.mentor.name}</p>
+                <h2>1 hour meeting</h2>
+                <h6>GMT {dataObj.mentor['time_zone']}</h6>
+              </div>
+              <div className="column is-8 card">
+                <Calendar data={calendarData} />
+              </div>
+              <div className="column is-2" />
             </div>
-            <div className="column is-2" />
           </div>
-        </div>
-      </section>
-    </div>
-  );
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
